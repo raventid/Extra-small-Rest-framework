0325d8245998ba57818fae59fcee5823aa47fcfd
raventid <raventid@users.noreply.github.com>
2015-05-27 11:22:14 +0300

    Update server.php

--- server.php
+++ server.php
@@ -36,8 +36,6 @@ try {
 $id = array_shift($request->getArgs());
 */
 
-//add better abstraction with pdo wrapper
-
 //*************************************************************************MAIN LOGIC**********************************************************************************
 
 switch ($request->getRequestMethod()){
